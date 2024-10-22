fc347322685bc9cc15b27761eb96167b625bba75
Tutorial <scobiciandrei95@gmail.com>
2024-10-22 12:30:31 +0200

    add initial backend project

---
+++ backend/src/main/java/scobici/andrei/rentcar/RentcarApplication.java
@@ -0,0 +1,13 @@
+package scobici.andrei.rentcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentcarApplication.class, args);
+	}
+
+}
